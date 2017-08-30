635dea15a054c5a8b201e58e05893a882303859e
Mirko Lecic <lecic.mirko@gmail.com>
2017-08-30 23:04:15 +0200

    Uputstvo kako raditi sa fork repozitorijumom i originalnim, tj. kako sinhronizovati rad.

---
+++ TestFolder/Uputstvo za fork.php
@@ -0,0 +1,8 @@
+<?php
+
+  echo "<ol>";
+  echo "<li>https://help.github.com/articles/configuring-a-remote-for-a-fork/</li>";
+  echo "<li>https://help.github.com/articles/syncing-a-fork/</li>";
+  echo "</ol>";
+
+ ?>
